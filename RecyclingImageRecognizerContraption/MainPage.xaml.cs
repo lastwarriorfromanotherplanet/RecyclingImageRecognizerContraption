e9165377b570f8bc2e578780f1dad73f3d6195d9
lastwarriorfromanotherplanet <mark.kadah@gmail.com>
2025-03-12 19:50:23 -0500

    Add project files.

---
+++ RecyclingImageRecognizerContraption/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RecyclingImageRecognizerContraption
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RecyclingImageRecognizerContraption/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RecyclingImageRecognizerContraption
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
