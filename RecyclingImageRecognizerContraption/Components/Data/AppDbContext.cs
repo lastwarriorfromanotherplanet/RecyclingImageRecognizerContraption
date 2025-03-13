cd3a150f7d25149cc5526b2e5fb6c642b6316e95
lastwarriorfromanotherplanet <mark.kadah@gmail.com>
2025-03-12 23:14:37 -0500

    Put in a camera that does nothing and a database that does nothing.

---
+++ RecyclingImageRecognizerContraption/Components/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecyclingImageRecognizerContraption.Components.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Homewood> HomewoodRecyclables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=mydatabase.db");
+}
+
+public class Homewood
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Category { get; set; }
+    public string? Instructions { get; set; }
+
+
+}
+
